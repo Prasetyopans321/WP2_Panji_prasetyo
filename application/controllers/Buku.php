9edeeec5a8e476c503965b8a245861edb88673d1
prasetyopans321 <prasetyopanji717@gmail.com>
2022-11-26 21:47:08 +0700

    tugas 12

--- application/controllers/Buku.php
+++ application/controllers/Buku.php
@@ -1,6 +1,5 @@
 <?php
-
-
+defined('BASEPATH') or exit('No direct script access allowed');
 class Buku extends CI_Controller
 {
     public function __construct()
@@ -8,74 +7,7 @@ class Buku extends CI_Controller
         parent::__construct();
         cek_login();
     }
-
-    // Region Kategori
-    public function kategori()
-    {
-        $data['judul'] = 'Kategori Buku';
-        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-        $data['kategori'] = $this->ModelBuku->getKategori()->result_array();
-        $this->form_validation->set_rules(
-            'kategori',
-            'Kategori',
-            'required',
-            [
-                'required' => 'Judul Buku harus diisi'
-            ]
-        );
-        if ($this->form_validation->run() == false) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('buku/kategori', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $data = ['kategori' => $this->input->post('kategori')];
-            $this->ModelBuku->simpanKategori($data);
-            redirect('buku/kategori');
-        }
-    }
-
-    public function ubahkategori()
-    {
-        $data['judul'] = 'Ubah Kategori Buku';
-        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-
-        $where = ['id' =>  $this->uri->segment(3)];
-        $data['kategori'] = $this->ModelBuku->kategoriWhere($where)->row_array();
-
-
-        $this->form_validation->set_rules(
-            'kategori',
-            'Kategori',
-            'required',
-            [
-                'required' => 'Judul Buku harus diisi'
-            ]
-        );
-
-        if ($this->form_validation->run() == false) {
-            $this->load->view('templates/header', $data);
-            $this->load->view('templates/sidebar', $data);
-            $this->load->view('templates/topbar', $data);
-            $this->load->view('buku/ubah_kategori', $data);
-            $this->load->view('templates/footer');
-        } else {
-            $data = ['kategori' => $this->input->post('kategori')];
-            $this->ModelBuku->updateKategori(['id' => $this->input->post('id')], $data);
-            redirect('buku/kategori');
-        }
-    }
-
-    public function hapusKategori()
-    {
-        $where = ['id' => $this->uri->segment(3)];
-        $this->ModelBuku->hapusKategori($where);
-        redirect('buku/kategori');
-    }
-
-
-    // Region Buku
+    //manjemen Buku
     public function index()
     {
         $data['judul'] = 'Data Buku';
